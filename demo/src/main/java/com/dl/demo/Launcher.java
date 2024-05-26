141bacc381226bfc9c44525394f74334b95cdd4f
dlenaPL <156719318+dlenaPL@users.noreply.github.com>
2024-05-26 19:06:58 +0200

    Delete demo directory

--- demo/src/main/java/com/dl/demo/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.dl.demo;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
-    }
-}
